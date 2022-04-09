b6b5f450d56c5ad9caf36374c3714c1503825c45
natch-non <90593501+natch-non@users.noreply.github.com>
2022-04-09 15:50:01 -0400

    nonononono
    from fred

---
+++ src/components/landing-page.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Landingpage extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+
+            </>
+        );
+    }
+}
+ 
+export default Landingpage;
