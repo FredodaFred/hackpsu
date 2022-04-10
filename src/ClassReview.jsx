4e115dd05761b04ef79c16779fd614ca7e023a32
Frederick Sion <FredodaFred@gmail.com>
2022-04-09 23:32:26 -0400

    set up for backend use

--- src/ClassReview.jsx
+++ src/ClassReview.jsx
@@ -1,10 +1,11 @@
 import { useLocation } from "react-router-dom";
+import React, {useState, useEffect } from "react";
 
 const ClassReview = () => {
   const location = useLocation();
   let subjectStr = String(location.state.subject);
   subjectStr = subjectStr.toUpperCase()
-  console.log(subjectStr)
+  //console.log(subjectStr)
 
   return (
     <>
@@ -12,8 +13,43 @@ const ClassReview = () => {
         <h1>Class Review</h1>
         <h1>{`${ subjectStr } ${location.state.number}`}</h1>
       </div>
+      <ReviewComponent/>
     </>
   );
 };
+
+const ReviewComponent = () =>{
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
+
+  return(
+    <>
+    </>
+  )
+}
 
 export default ClassReview;
