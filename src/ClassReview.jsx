70e3730629495d454884cf5eab27ad006023cce7
Frederick Sion <FredodaFred@gmail.com>
2022-04-09 23:26:05 -0400

    some stuff

--- src/ClassReview.jsx
+++ src/ClassReview.jsx
@@ -2,11 +2,15 @@ import { useLocation } from "react-router-dom";
 
 const ClassReview = () => {
   const location = useLocation();
+  let subjectStr = String(location.state.subject);
+  subjectStr = subjectStr.toUpperCase()
+  console.log(subjectStr)
+
   return (
     <>
       <div className="d-flex flex-column align-items-center">
         <h1>Class Review</h1>
-        <h1>{`${location.state.subject} ${location.state.number}`}</h1>
+        <h1>{`${ subjectStr } ${location.state.number}`}</h1>
       </div>
     </>
   );
